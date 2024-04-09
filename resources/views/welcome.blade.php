868daef70bbcf52061f23cf404203d2d8827b775
imantnttelzrs <antonettelozares6@gmail.com>
2024-04-09 12:15:14 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column align-items-center justify-content-center">
-    <h1>Hi, I'm Antonette</h1>
+    @foreach($user as $users)
+    <h1>Hi, I'm {{ucwords($users->name)}}</h1>
     <h2>Welcome to my portfolio</h2>
     <a href="#about" class="btn-get-started scrollto"><i class="bi bi-chevron-double-down"></i></a>
   </section><!-- End Hero -->
@@ -47,8 +48,10 @@
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
       <div class="logo">
-        <h1><a href="#about">ANTONETTE</a></h1>
+        <h1><a href="#about">{{strtoupper($users->name)}}</a></h1>
+        @endforeach
       </div>
+
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
