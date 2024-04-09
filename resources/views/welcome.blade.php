3e6d441c1bd614afc753bc5bf7afbd8e8f709ef8
imantnttelzrs <antonettelozares6@gmail.com>
2024-04-09 13:24:06 +0800

    udpate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,14 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/icon.jpg" rel="icon">
-  <link href="assets/img/icon.jpg" rel="apple-touch-icon">
+  <link href='{{asset("assets/img/icon.jpg")}}' rel="icon">
+  <link href="{{asset("assets/img/icon.jpg")}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i&subset=cyrillic" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href='{{asset("assets/vendor/bootstrap/css/bootstrap.min.css")}}' rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
